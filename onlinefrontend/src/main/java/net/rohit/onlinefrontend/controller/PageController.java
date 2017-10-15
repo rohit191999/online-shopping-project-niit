99c462332f68e41b151c8965ad2fcc3fb0c50518
Rohit Pose <rohitpose750gmail.com>
2017-10-15 15:54:42 +0530

    spring config complete

---
+++ onlinefrontend/src/main/java/net/rohit/onlinefrontend/controller/PageController.java
@@ -0,0 +1,19 @@
+package net.rohit.onlinefrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class  PageController     {
+
+	
+	
+	
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("gretting","WELCOME TO SPRING WEB MVC");
+		return mv;
+	}
+}
